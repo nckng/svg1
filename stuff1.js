4b8b9c9f5300bddbbbc44f9976d4b4e1fc107af2
nckng <nng3@stuy.edu>
2017-02-28 23:43:19 -0500

    work

--- stuff1.js
+++ stuff1.js
@@ -7,16 +7,40 @@ var drawDot = function(e){
     var x = e.offsetX;
     var y = e.offsetY;
     if (oldx == -1 && oldy == -1){
-	var ins = document.createElementNS("https://www.w3.org/2000/svg","circle");
-	ins.setAttribute("cx","5");
-	ins.setAttribute("cy","5");
-	ins.setAttribute("fill","DarkOrchid");
-	ins.setAttribute("r","5");
+	var ins = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	ins.setAttribute("cx",x);
+	ins.setAttribute("cy",y);
+	ins.setAttribute("fill","blue");
+	ins.setAttribute("r","20");
 	canvas.appendChild(ins);
+	oldx = x;
+	oldy = y;
     }
     else{
-	console.log("done");
+	var ins = document.createElementNS("http://www.w3.org/2000/svg","line");
+	ins.setAttribute("x1",oldx);
+	ins.setAttribute("y1",oldy);
+	ins.setAttribute("x2",x);
+	ins.setAttribute("y2",y);
+	ins.setAttribute("style","stroke: red");
+	canvas.appendChild(ins);
+	ins = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	ins.setAttribute("cx",x);
+	ins.setAttribute("cy",y);
+	ins.setAttribute("fill","blue");
+	ins.setAttribute("r","20");
+	canvas.appendChild(ins);
+	oldx = x;
+	oldy = y;
     };
-}
+};
 
-clear.addEventListener("click", drawDot);
+var clere = function(e){
+    oldx = -1;
+    oldy = -1;
+    canvas.innerHTML = "";
+};
+    
+
+canvas.addEventListener("click", drawDot);
+clear.addEventListener("click", clere);
